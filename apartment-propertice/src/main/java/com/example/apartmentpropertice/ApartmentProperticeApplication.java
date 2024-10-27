873a174415a42ae9b79dbca6ac0b3b933b771bec
rabiul islam <rabiulnewsinfo@gmail.com>
2024-10-27 17:17:42 +0600

    project initilaization by Rabiul islam

---
+++ apartment-propertice/src/main/java/com/example/apartmentpropertice/ApartmentProperticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.apartmentpropertice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentProperticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApartmentProperticeApplication.class, args);
+    }
+
+}
---
+++ apartment-propertice/src/test/java/com/example/apartmentpropertice/ApartmentProperticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartmentpropertice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentProperticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
